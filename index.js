8993021208f8c8e37ba34a24564256462d785261
Cool Ambast <83899058+CoolAmbast@users.noreply.github.com>
2026-09-07 12:42:45 +0530

    main layout base inti

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 import express from 'express';
 import path from 'path';
+import expressLayouts from 'express-ejs-layouts';
 
 const app = express();
-
+// uhhhh.....express layout setup ig
+app.use(expressLayouts);
 app.use(express.static(path.resolve('public')));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('src', 'views'));
+app.set('layout', 'homeLayout');
 
-/*app.get('/', (req, res) => {
-  res.render('home');
-});*/
 
+app.get('/contact', (req, res) => {
+  res.render('contact', { cssFile: 'contact' });
+});
 app.get('/', (req, res) => {
-  res.render('portfolioForm');
+  res.render('home');
+});
+app.get('/form', (req, res) => {
+  res.render('portfolioForm', { cssFile: 'portfolioFormStyle' });
 });
 
+
+// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
